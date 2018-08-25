d9091755d983995d233dc405907620cdee593dc7
Mikala <mikalacesmith@gmail.com>
2018-08-24 23:53:12 -0400

    orglanding page

--- src/components/OrgLanding.js
+++ src/components/OrgLanding.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import OrgNavigation from './OrgNavigation';
+import { Collapsible } from "./collapsible";
+import { Footer } from "./Footer.js";
+import "./Landing.css"
 
 
 const OrgLandingPage = () =>
   <div>
     <OrgNavigation />
-    <h1>Landing Page</h1>
+    <div className="jumbotron">
+      <h1 className="display-4">Welcome To Pay it Forward</h1>
+      <hr className="my-4" />
+      <h4>A place to connect volunteers with volunteer opportunties.</h4>
+      <a className="btn btn-success btn-lg" href="#" role="button">Get Involved!</a>
+    </div>
+    <Collapsible />
+    <Footer />
   </div>
 
 export default OrgLandingPage;
